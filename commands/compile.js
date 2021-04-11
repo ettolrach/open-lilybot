3604ec510698036cb46e67d3ea30450af2dbf27b
Charlotte Ausel <ettolrach@disroot.org>
2021-04-11 15:02:09 +0100

    Add further comments

--- commands/compile.js
+++ commands/compile.js
@@ -11,14 +11,11 @@ module.exports = {
     usage: `\`${config.prefix} [compile] FILE [OPTION]...\` Note that you don't need ot write "compile". That means you can also type \`${config.prefix} FILE [OPTIONS]...\``,
     options: `\`$v\`, \`$$verbose\`: include Lilypond's output from the terminal when compilation is finished. **NOTE** that the options should come after the file (code), not before!`,
 	execute(message, args) {
+        // Remove "compile" if the user specified it so that it doesn't get passed into the Lilypond command.
         if (args[0].substring(0, 7) == "compile") {
             args[0] = args[0].substring(7);
         }
-        console.log("---");
-        for (let i = 0; i < args.length; i++) {
-            console.log(args[i]);
-        }
-        console.log("---");
+
         // Write the code to a file.
         fs.writeFileSync("code.ly", args[1]);
         // If the output folder doesn't already exist, create it.
@@ -26,7 +23,9 @@ module.exports = {
             fs.mkdirSync("./generatedFiles");
         }
 
+        // Run the Lilypond command to compile the code.
         const lilypond = exec("lilypond " + args[0] + " --output=generatedFiles code.ly ", (error, stdout, stderr) => {
+            // Report an error if one occured.
             if (error) {
                 console.log(error.stack);
                 console.log("Error code: " + error.code)
@@ -37,9 +36,10 @@ module.exports = {
             }
             
             // Get the most recent file in the folder and upload that one.
+            // First get all files in the folder.
             let files = fs.readdirSync("./generatedFiles");
 
-            // Use this to track the current newest files.
+            // Then use this to track the current newest files.
             let currentLatestTime = new Date(0);
             let latestTimePosition = 0;
             for (let i = 0; i < files.length; i++) {
@@ -51,6 +51,7 @@ module.exports = {
             
             // Send the generated file.
             message.channel.send("Generated file:", { files: [`./generatedFiles/${files[latestTimePosition]}`] })
+            
             // Also send the terminal output if $v was specified.
             if (args[2] == "$$verbose" || args[2] == "$v") {
                 message.channel.send("Output: ```" + stderr + "```");
