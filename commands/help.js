cf97a04f61e244fdda53896afd17a0bc514459ff
ettolrach <ettolrach@disroot.org>
2021-11-14 02:02:29 +0000

    Improve help command

--- commands/help.js
+++ commands/help.js
@@ -1,5 +1,5 @@
 // Load the configuration.
-const config = require("../config.json");
+const { prefix } = require("../config.json");
 
 module.exports = {
     name: "help",
@@ -9,25 +9,29 @@ module.exports = {
     execute(message, args) {
         if (args.length == 0) {
             message.channel.send(
-`**__List of commands__**
-· \`about\`, \`version\`, \`-v\` Show the version of the bot and Lilypond.
-· \`compile\` Compile Lilypond code. This command is most likely the one which you would use most frequently.
-· \`help\` Show this list of commands and get more information about specific commands.
+`__**List of commands**__
+· \`about\` Show the version of the bot and of Lilypond.
+· \`compile\` Compile Lilypond code.
+· \`help\` Show this list of commands and get more information about speific commands.
 · \`ping\` Check if the bot is online and get ponged back.
 
-You can type \`${config.prefix} help COMMAND\` to get help about a specific command.`
+You can type \`\`${prefix} help [COMMAND]\` to get information about a specific command.`
             );
             return;
         }
 
         switch (args[0]) {
             case "about":
-                message.channel.send(`Usage: \`${config.prefix} about\`\n\nDescription:Show the version of the bot and Lilypond. You can visit this link to see the latest version. https://github.com/ettolrach/open-lilybot/releases`);
+                message.channel.send(
+`**Usage:** \`${prefix} about\`
+
+**Description:** Show the version of the bot and of Lilypond.`
+                );
                 break;
 
             case "compile":
                 message.channel.send(
-`**Usage:** \`${config.prefix} [compile] [OPTION]... FILE\` Note that you don't need to include "compile", as shown in the example below.
+`**Usage:** \`${prefix} [compile] [OPTION]... FILE\` Note that you don't need to include "compile", as shown in the example below.
 
 **Description:** Compile some Lilypond code. You can include options as detailed below.
 
@@ -36,26 +40,41 @@ You can type \`${config.prefix} help COMMAND\` to get help about a specific comm
 · \`verbose\`: run the command with the -dverbose flag. This can be useful if you have made a mistake and don't know what's wrong.
 
 **Example:**
-${config.prefix} preview
+${prefix} preview
 \\\`\\\`\\\`\\relative c'' {
     \\clef "treble"
     \\time 4/4
     c2 e4 g
     b,4. c16 d c4 r4
 }\\\`\\\`\\\``
-                    );
+                );
                 break;
 
             case "help":
-                message.channel.send(`Usage: \`${config.prefix} help [COMMAND NAME]...\`\nDescription: View the list of commands. You can also specify a specific command after \`help\` so you can view more detailed information about a specific command, just like you did now.`);
+                message.channel.send(
+`**Usage:** \`${prefix} help [COMMAND NAME]\`
+
+
+**Description:** View the list of commands. If a command name is specified, you will get more detailed information about it.
+
+**Commands:**
+· about
+· compile
+· help
+· ping`
+                );
                 break;
 
             case "ping":
-                message.channel.send(`Usage: \`${config.prefix} ping\`\nDescription: This will send back a message as soon as possible. This command is mainly used to check if the bot is online.`);
+                message.channel.send(
+`**Usage:** \`${prefix} ping\`
+
+**Description:** This will send back a message as soon as possible. Use this to check if the bot is online.`
+                );
                 break;
         
             default:
-                message.channel.send(`That command does not exist. Run \`${config.prefix} help\` to view all the commands.`);
+                message.channel.send(`That command does not exist. Run \`${prefix} help\` to view all the commands.`);
                 break;
         }
     },
