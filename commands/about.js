56829c8802cca8cc41f8d29c74e40af603f23e1a
ettolrach <ettolrach@disroot.org>
2021-11-14 01:51:44 +0000

    Use proper Promises

--- commands/about.js
+++ commands/about.js
@@ -5,23 +5,29 @@ module.exports = {
     name: "about",
     description: "Show version information about the bot and Lilypond.",
     aliases: ["-v", "version"],
-    execute(message, args) {
-        toSend = "Open Lilybot 1.1.1\n";
+    execute(message) {
+        let toSend = "Open Lilybot 1.1.1\n";
 
-        const lilypond = exec("lilypond -v", (error, stdout, stderr) => {
-            if (error) {
-                console.log(error.stack);
-                console.log("Error code: " + error.code)
-                console.log("Signal received: " + error.signal);
-                console.log("Time of error: " + String(Date()));
-                message.channel.send("An error has occured while attempting to get the version number of Lilypond (maybe check whether Lilypond is installed on the server?): ```" + error.stack + "```");
-                return;
-            }
+        const lilypondVersionCommand = new Promise((resolve, reject) => {
+            exec("lilypond -v", (error, stdout, stderr) => {
+                if (error) {
+                    console.log(error.stack);
+                    console.log("Error code: " + error.code)
+                    console.log("Signal received: " + error.signal);
+                    console.log("Time of error: " + String(Date()));
+                    reject(err);
+                    return;
+                }
+                resolve(stdout.split("\n")[0]);
+            });
+        });
 
-            toSend = toSend + stdout.split("\n")[0];
-            toSend = toSend + "\nOpen Source, view the code at <https://github.com/ettolrach/open-lilybot/>";
-            console.log(toSend);
+        lilypondVersionCommand.then(async lilypondOutput => {
+            toSend += lilypondOutput + "\nThis project is open source, view the code at <https://github.com/ettolrach/open-lilybot/>.";
             message.channel.send(toSend);
+        }).catch(async err => {
+            console.log(err);
+            message.channel.send("An error has occured while attempting to get the version number of Lilypond (maybe check whether Lilypond is installed on the server?): ```" + error.stack + "```");
         });
     },
 };
